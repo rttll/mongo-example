89725345f19b275ec8005d5c8cc37f22a39a3f70
Adro <acousens@gmail.com>
2021-01-19 15:41:26 -0800

    Add basic routes setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,36 @@
 import { useState } from 'react'
 import { Frame, Scroll, useCycle } from "framer"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './Home'
+import Login from './Login'
+
 import './App.css';
-import { Slider } from './Slider'
 
 
 function App() {
-  const [scale, setScale] = useState(0.5)
-  const constraint = (480 * scale - 120) / 2
   return (
-    <Frame
-      name={"SliderApp"}
-      width={"100%"}
-      height={"100%"}
-      background={"#242424"}
-      >
-      <Frame
-        name={"Mask"}
-        size={120}
-        center 
-        y={"-100px"}
-        overflow={"hidden"}
-        radius={"50%"}
-        className={"bg-red-400"}
-        background={"#f4f4f4"}
-        >
-
-        <Frame
-          name={"Image"}
-          scale={scale}
-          center
-          size={480}
-          image={"https://static.framer.com/api/bg.jpg"}
-          drag
-          dragElastic={0}
-          dragMomentum={false}
-          dragConstraints={{
-            top: -constraint,
-            right: constraint, 
-            bottom: constraint,
-            left: -constraint
-          }}
-        />
-      </Frame>
-      <Slider
-        value={scale}
-        min={0.25}
-        max={0.75}
-        onChange={function(newValue) {
-          setScale(newValue)
-        }}
-       />
-    </Frame>
+    <Router>
+      <div className="flex flex-col items-center w-screen h-screen">
+        <header className="flex py-4">
+          <Link className="p-4 cursor-pointer hover:bg-red-300" to="/">Home</Link>
+          <Link className="p-4 cursor-pointer hover:bg-red-300" to="/login">Login</Link>
+        </header>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: [
+    'src/**/*.js',
+    'src/**/*.css',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
